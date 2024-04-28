b85bb8fead8cb69dcfc7f58b6b2df3b9ab62c191
Pradeep Dahal <pradeepdahal@alpha-2.local>
2024-04-28 21:38:34 +0545

    tags try

---
+++ src/components/Test.tsx
@@ -0,0 +1,4 @@
+const TestPage: React.FC = () => {
+  return <div>Test Page</div>;
+};
+export default TestPage;
--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,18 @@ import "./index.css";
 import "leaflet/dist/leaflet.css";
 import "react-leaflet-markercluster/dist/styles.min.css";
 import { MapProvider } from "./context/mapContext.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import TestPage from "./components/Test.tsx";
 
 ReactDOM.render(
   <React.StrictMode>
     <MapProvider>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/test" element={<TestPage />} />
+        </Routes>
+      </BrowserRouter>
     </MapProvider>
   </React.StrictMode>,
   document.getElementById("root")
